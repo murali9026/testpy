ca9b3f00135fb96a737ffab4e23c76fab0612d56
murali9026 <murali.krishna@opsera.io>
2024-10-02 23:43:57 -0700

    Update test_psycopg2.sh

--- test_psycopg2.sh
+++ test_psycopg2.sh
@@ -2,5 +2,5 @@
 
 apt-get update
 apt-get install -y python3-pip
-apt-get install -y python3-psycopg2  
+pip3 install psycopg2-binary  
 python3 -c "import psycopg2; print('psycopg2 imported successfully')"
